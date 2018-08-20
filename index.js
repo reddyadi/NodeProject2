d1a747a4a690a7be0e69aec7d85ffaf173d3cceb
Aditya REddy <aditya.reddy@hotmail.co.nz>
2018-08-21 11:36:59 +1200

    path links

--- index.js
+++ index.js
@@ -1,11 +1,41 @@
 const http = require('http');
+const fs = require('fs');
+const path = require('path');
 
 var server = http.createServer(function(request, response){
+  console.log(`${request.method} request for ${request.url}`);
+  var page;
+  if(request.url === "/"){
+    fs.readFile('./public/home.html', 'UTF-8', function(error, contents){
+      if(error){
+        console.log("error, something went wrong");
+      } else {
+        response.writeHead(200, {'Content-Type':'text/html'});
+        response.end(contents);
+      }
+    });
+  } else if (request.url.match(/.css$/)){
+    var cssPath = path.join(__dirname, 'public', request.url);
+    var fileStream = fs.createReadStream(cssPath, 'UTF-8');
+    console.log(cssPath);
+    response.writeHead(200, {'Content-Type': 'text/css'});
+    fileStream.pipe(response);
+  }
 
-  response.writeHead(200, {'Content-Type': 'text/plain'});
-  response.end('This is Running from a server, changed the file using nodemon');
+  // else if(request.url === "/contact"){
+  //   page = "contact";
+  // } else if(request.url === "/about"){
+  //   page = "about";
+  // } else if (request.url === "/front"){
+  //   page = "front";
+  // } else {
+  //   page = "404 page not found";
+  // }
 
+  // response.writeHead(200, {'Content-Type': 'text/html'});
+  // response.end();
 });
+
 server.listen(3000);
 
 console.log("The server is running on port 3000");
