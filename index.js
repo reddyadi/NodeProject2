0f6df8d6efeb54c6d75277f8aa29a29ffc5aa040
Aditya REddy <aditya.reddy@hotmail.co.nz>
2018-08-21 16:59:51 +1200

    fs links

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const fs = require('fs');
 const path = require('path');
 
 var server = http.createServer(function(request, response){
+
   console.log(`${request.method} request for ${request.url}`);
   var page;
   if(request.url === "/"){
@@ -20,6 +21,42 @@ var server = http.createServer(function(request, response){
     console.log(cssPath);
     response.writeHead(200, {'Content-Type': 'text/css'});
     fileStream.pipe(response);
+  } else if (request.url === "/about"){
+    fs.readFile('./public/about.html', 'UTF-8', function(error, contents){
+      if(error){
+        console.log("error, something went wrong");
+      } else {
+        response.writeHead(200, {'Content-Type': 'text/html'});
+        response.end(contents);
+      }
+    });
+  } else if (request.url === "/contact"){
+    fs.readFile('./public/contact.html', 'UTF-8', function(error, contents){
+      if(error){
+        console.log("error, something went wrong");
+      } else {
+        response.writeHead(200, {'Content-Type': 'text/html'});
+        response.end(contents);
+      }
+    });
+  } else if (request.url === "/front"){
+    fs.readFile('./public/front.html', 'UTF-8', function(error, contents){
+      if(error){
+        console.log("error, something went wrong");
+      } else {
+        response.writeHead(200, {'Content-Type': 'text/html'});
+        response.end(contents);
+      }
+    });
+  } else if (request.url.match(/.img$/)){
+    var imgPath = path.join(__dirname, 'public', request.url);
+    var fileStream = fs.createReadStream(imgPath, 'UTF-8');
+    console.log(imgPath);
+    response.writeHead(200, {'Content-Type': 'text/css'});
+    fileStream.pipe(response);
+  }
+   else {
+    page = "404 page not found";
   }
 
   // else if(request.url === "/contact"){
